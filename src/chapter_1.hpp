2d8b57b77fa41b79fa5f0f1dcd0f6bc31e8068ac
brian <brian@gamma>
2025-08-08 12:02:06 -0400

    triplets

--- src/chapter_1.hpp
+++ src/chapter_1.hpp
@@ -1,3 +1,4 @@
+#include <algorithm> // std::sort
 #include <utility> // std::pair
 #include <vector>
 
@@ -18,12 +19,12 @@ pair_sum_sorted(std::vector<int> const& nums, int target)
         // if the sum is smaller, increment the left pointer, aiming to
         // increase the sum toward the target value
         if (sum < target) {
-            left += 1;
+            ++left;
         }
         // if the sum is larger, decrement the right pointer, aiming to
         // decrease the sum toward the target value
         else if (sum > target) {
-            right -= 1;
+            --right;
         }
         // if the target pair is found, return its indexes
         else {
@@ -31,5 +32,71 @@ pair_sum_sorted(std::vector<int> const& nums, int target)
         }
     }
     return {};
+}
+
+// Given an array of integers, return all triplets [a, b, c] such that
+// a + b + c = 0. The solution must not contain duplicate triplets
+// (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicate triplets). If
+// no such triplets are found, return an empty array.
+std::vector<std::pair<int, int>> pair_sum_sorted_all_pairs(
+        std::vector<int> const& nums, int start, int target);
+
+struct triplet
+{
+    int a, b, c;
+};
+std::vector<triplet>
+triplet_sum(std::vector<int> const& nums)
+{
+    std::vector<triplet> triplets;
+    auto sorted_nums = nums;
+    std::sort(sorted_nums.begin(), sorted_nums.end());
+
+    for (int i = 0; i < static_cast<int>(sorted_nums.size()); ++i) {
+        // optimization: triplets consisting of only positive numbers
+        // will never sum to 0
+        if (sorted_nums[i] > 0) {
+            break;
+        }
+        // to avoid duplicate triplets, skip 'a' if it's the same as the
+        // previous number
+        if (i > 0 && sorted_nums[i] == sorted_nums[i - 1]) {
+            continue;
+        }
+        // find all pairs that sum to a target of '-a' (-sorted_nums[i])
+        auto pairs = pair_sum_sorted_all_pairs(sorted_nums, i + 1, -sorted_nums[i]);
+        for (auto& pair : pairs) {
+            triplet t{sorted_nums[i], pair.first, pair.second};
+            triplets.emplace_back(t);
+        }
+    }
+    return triplets;
+}
+
+std::vector<std::pair<int, int>>
+pair_sum_sorted_all_pairs(std::vector<int> const& nums, int start, int target)
+{
+    std::vector<std::pair<int, int>> pairs;
+    int left = start;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int sum = nums[left] + nums[right];
+        if (sum == target) {
+            pairs.emplace_back(std::pair{nums[left], nums[right]});
+            ++left;
+            // to avoid duplicate '[b, c]' pairs, skip 'b' if it's the
+            // same as the previous number
+            while (left < right && nums[left] == nums[left - 1]) {
+                ++left;
+            }
+        } else if (sum < target) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+
+    return pairs;
 }
 
--- tests/test_chapter_1.cpp
+++ tests/test_chapter_1.cpp
@@ -7,6 +7,7 @@ TEST_CASE("chapter 1", "[chapter_1]")
     {
         REQUIRE(pair_sum_sorted({-5, -2, 3, 4, 6}, 7) == std::vector{2, 3});
         REQUIRE(pair_sum_sorted({1, 1, 1}, 2) == std::vector{0, 2});
+
         REQUIRE(pair_sum_sorted({}, 0) == std::vector<int>{});
         REQUIRE(pair_sum_sorted({1}, 1) == std::vector<int>{});
         REQUIRE(pair_sum_sorted({2, 3}, 5) == std::vector{0, 1});
@@ -17,5 +18,17 @@ TEST_CASE("chapter 1", "[chapter_1]")
     }
 
     SECTION("Triplet Sum")
-    {}
+    {
+        auto triplets = triplet_sum({0, -1, 2, -3, 1});
+        REQUIRE(triplets[0].a == -3);
+        REQUIRE(triplets[0].b == 1);
+        REQUIRE(triplets[0].c == 2);
+        REQUIRE(triplets[1].a == -1);
+        REQUIRE(triplets[1].b == 0);
+        REQUIRE(triplets[1].c == 1);
+
+        REQUIRE(triplet_sum({0, -1, 2, -3, 1}) == std::vector<triplet>{{-3, 1, 2}, {-1, 0, 1}});
+
+        REQUIRE(triplet_sum({}).empty());
+    }
 }
